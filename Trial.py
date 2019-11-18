a8b554dfb2c7b0c0acff6cb13e2424c88d363221
Rochan90 <52199601+Rochan90@users.noreply.github.com>
2019-11-18 13:39:25 +0000

    Add files via upload

--- Trial.py
+++ Trial.py
@@ -1,8 +1,52 @@
+import sqlite3
 import random
 
-global arrival
-for y in range(100):
-    arrival = round(random.uniform(0, 1), 2)
-    print(arrival)
+connection = sqlite3.connect("Data.db")
+cursor = connection.cursor()
 
+tableData = "SELECT * FROM SimData ORDER BY Arrival"
+tableWord = "SELECT ID FROM SimData ORDER BY Arrival"
+tableArrival = "SELECT Arrival FROM SimData ORDER BY Arrival"
+tableDuration = "SELECT Duration FROM SimData ORDER BY Arrival"
+
+cursor.execute(tableData)
+data = cursor.fetchall()
+
+cursor.execute(tableWord)
+word = cursor.fetchall()
+
+cursor.execute(tableArrival)
+arrivalTime = cursor.fetchall()
+
+cursor.execute(tableDuration)
+taskDuration = cursor.fetchall()
+
+
+'''for d in word:
+    c = str(d).strip("(),''")
+    print(c)'''
+
+'''for s in arrivalTime:
+    t = str(s).strip("(),")
+    print(t)'''
+
+'''for b in taskDuration:
+    e = str(b).strip("(),")
+    print(e)'''
+
+for x in range(100):
+    #ran = round(random.uniform(0, 1), 2)
+    #ran = round(random.random(), 2)
+    ran = round(random.expovariate(1), 2)
+    print(ran)
+
+
+# length = len(data)
+"""print(word)
+print(arrivalTime)
+print(taskDuration)"""
+
+
+'''for a in data:
+    print(a)'''
 
